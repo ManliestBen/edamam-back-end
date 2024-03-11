59e1c07e648629692def6328e0281f80a8add2d8
Ben Manley <benjamintmanley@gmail.com>
2024-03-11 10:03:34 -0500

    protect sensitive information from reaching front end

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,9 +1,16 @@
 import { Recipe } from "../models/recipe.js"
-
+const edamamUrl = `https://api.edamam.com/api/recipes/v2`
 
 async function search(req, res) {
-  console.log(req.body)
-  res.json([{recipeName: 'TEST'}, {recipeName: 'TEST2'}])
+  try {
+    const apiResponse = await fetch(`${edamamUrl}?type=public&q=${req.body.query}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`)
+    const apiData = await apiResponse.json()
+    apiData.hits.forEach(recipe => delete recipe['_links'])
+    res.json(apiData.hits)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
 }
 
 export {
